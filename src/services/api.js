7b8edb5e83c5248098e3d26845f45185817fbe62
JoaoChrisostomo <maturandomassas@gmail.com>
2022-07-05 16:15:19 -0300

    ajustando o arquivo api.js colocando o https

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 // URL DA API: /movie/now_playing?api_key=6a147fefcedfb3e27ff6a2dab4910495&language=pt-BR
 
 const api = axios.create({
-  baseURL: 'http://api.themoviedb.org/3/'
+  baseURL: 'https://api.themoviedb.org/3/'
 })
 
 export default api
